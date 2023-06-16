5169d4a0e7c50133c0f8edd6bc4aa932032ac7e9
okojomoeko <chikoro.size@gmail.com>
2023-06-16 18:19:05 +0900

    karabiner

--- link.sh
+++ link.sh
@@ -1,14 +1,21 @@
 IGNORE_PATTERN="^\.(git|config)"
 
-echo "Create dotfile links."
-for dotfile in .??*; do
-  [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
-  ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
-done
+# echo "Create dotfile links."
+# for dotfile in .??*; do
+#   [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+#   ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+# done
 
 
 mkdir -p $HOME/.config
-for dotfile in "$(ls .config)"; do
+for dotfile in $(ls -1 .config); do
   [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
-  ln -snfv "$(pwd)/.config/$dotfile" "$HOME/.config/$dotfile"
+  if [ -d "$HOME/.config/$dotfile" ]; then
+    ln -snfv "$(pwd)/.config/$dotfile/" "$HOME/.config/$dotfile"
+
+  else
+    ln -snfv "$(pwd)/.config/$dotfile" "$HOME/.config/$dotfile"
+  fi
+  # echo "$(pwd)/.config/$dotfile"
+  # echo "$HOME/.config/$dotfile"
 done
